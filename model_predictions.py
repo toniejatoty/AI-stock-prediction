6baba375b2ce354fd08ac998cb12840bf9c29bb0
Konrad Wołkiewicz <konrad.wolkiewicz@o2.pl>
2025-03-13 20:20:41 +0100

    program not is finding the best amount of days in past to train

---
+++ model_predictions.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+
+#M model
+M_LinearRegressor = LinearRegression
+M_RandomForestRegressor = RandomForestRegressor
+ 
