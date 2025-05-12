fixed data leak in linear

model_LinearRegression.py
@@ -4,74 +4,34 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 def predict_stock_prices(df_org, days_in_future_to_predict, loss_function):
     Status="OK"
-    infinity = float('inf')
-    max_size_df = df_org.shape[0]
-    days_in_past_to_train = {}
+    
+    #to test model   
+    days_to_train_testing = get_best_num_of_days_to_train(df_org[0:-days_in_future_to_predict], days_in_future_to_predict,loss_function)
+    
+    _,_,result_of_testing=get_prediction(df_org.iloc[0:-days_in_future_to_predict],days_to_train_testing,days_in_future_to_predict)
+    score = get_score(result_of_testing, df_org['Close'].iloc[-2*days_in_future_to_predict:-days_in_future_to_predict], loss_function)
+    
+    days_to_train_pred = get_best_num_of_days_to_train(df_org, days_in_future_to_predict,loss_function)
+    #to predict future
+    _,_,future_pred=get_prediction(df_org,days_to_train_pred,days_in_future_to_predict)
+    print(days_to_train_testing)
+    return ( result_of_testing,future_pred,days_to_train_pred,score, Status )
 
-    days_in_past_to_train.update({
-            i: infinity
-            for i in range(max_size_df - days_in_future_to_predict, 1000, -1000)
-        })
 
-    days_in_past_to_train.update({
-            i: infinity
-            for i in range(min(1000, max_size_df - days_in_future_to_predict), 200, -50)
-        })
 
-    days_in_past_to_train.update({
-            i: infinity
-            for i in range(min(200, max_size_df - days_in_future_to_predict), 50, -10)
-        })
-    days_in_past_to_train.update({i: infinity for i in range(min(50, max_size_df - days_in_future_to_predict), 10, -5)})
+def get_prediction(df_org, days_to_train, days_in_future_to_predict):
     model = LinearRegression()
-    for days_to_train in days_in_past_to_train:
-        predictions, y_test = get_prediction(
-            df_org,
-            days_to_train,
-            days_in_future_to_predict,
-            model,
-            days_in_future_to_predict,
-        )
-
-        score = get_score(y_test, predictions, loss_function)
-        days_in_past_to_train[days_to_train] = score
-
-    days_to_train = min(days_in_past_to_train, key=lambda k: days_in_past_to_train[k])
-    score_of_training = days_in_past_to_train[days_to_train]
-    # to visualize result of training
-    result_of_testing_to_visualize, _ = get_prediction(
-        df_org,
-        days_to_train,
-        days_in_future_to_predict,
-        model,
-        days_in_future_to_predict,
-    )
-    # to predict in future
-
-    result_of_predictions, _ = get_prediction(
-        df_org, days_to_train, 0, model, days_in_future_to_predict
-    )
-    return (
-        result_of_testing_to_visualize,
-        result_of_predictions,
-        days_to_train,
-        score_of_training,
-        Status
-    )
-
-
-def get_prediction(
-    df_org, days_to_train, days_in_future_to_test_model, model, days_in_future_to_predict
-):
-    df = df_org.tail(days_to_train + days_in_future_to_test_model).copy()
+    df=df_org.copy()
     X_train = np.array(range(0, days_to_train)).reshape(-1, 1)
-    X_test = np.array(
-        range(days_to_train, days_to_train + days_in_future_to_predict)
-    ).reshape(-1, 1)
-    y_train = df["Close"].iloc[0:days_to_train]
-    y_test = df["Close"].iloc[days_to_train : days_to_train + days_in_future_to_test_model]
+    y_train = df["Close"].iloc[-(days_to_train+days_in_future_to_predict): -days_in_future_to_predict]
+    X_test = np.array(range(days_to_train, days_to_train+days_in_future_to_predict)).reshape(-1, 1)
+    y_test = df["Close"].iloc[-days_in_future_to_predict:]
     model.fit(X_train, y_train)
-    return model.predict(X_test), y_test
+
+    X_future = np.array(range(days_to_train+days_in_future_to_predict, days_to_train+(2*days_in_future_to_predict))).reshape(-1, 1)
+    modelfuture = LinearRegression()
+    modelfuture.fit(X_test, y_test)
+    return model.predict(X_test), y_test, modelfuture.predict(X_future)
 
 def get_score(y_test, predictions, loss_function):
         if loss_function == "mse":
@@ -79,3 +39,22 @@ def get_score(y_test, predictions, loss_function):
         elif loss_function == "mae":
             score = mean_absolute_error(y_test, predictions)
         return score
+
+def get_best_num_of_days_to_train(df_org, days_in_future_to_predict,loss_function):
+    infinity = float('inf')
+    max_size_df = df_org.shape[0]
+    days_in_past_to_train = {}
+    days_in_past_to_train.update({i: infinity for i in range(max_size_df - days_in_future_to_predict, 1000, -1000)})
+    days_in_past_to_train.update({i: infinity for i in range(min(1000, max_size_df - days_in_future_to_predict), 200, -50)})
+    days_in_past_to_train.update({i: infinity for i in range(min(200, max_size_df - days_in_future_to_predict), 50, -10)})
+    days_in_past_to_train.update({i: infinity for i in range(min(50, max_size_df - days_in_future_to_predict), 10, -5)})
+    days_in_past_to_train.update({i: infinity for i in range(min(10, max_size_df - days_in_future_to_predict), 1, -1)})
+    for days_to_train in days_in_past_to_train:
+        x_train_pred, y_train, future_pred = get_prediction(df_org,days_to_train,days_in_future_to_predict)
+
+        score = get_score(x_train_pred, y_train, loss_function)
+        days_in_past_to_train[days_to_train] = score
+
+    days_to_train = min(days_in_past_to_train, key=lambda k: days_in_past_to_train[k])
+
+    return days_to_train

model_XGBRegressor.py
@@ -12,30 +12,9 @@ def predict_stock_prices(
     progress_callback
 ):
     Status="OK"
-    df = df_org.copy()
-    required_cols = df.columns
-    for i in range(1, days_in_future + 1):
-        df[f'target_{i}'] = df['Close'].shift(-i)
-    df.dropna(inplace=True)
 
-    #get training and test data
-    X = []
-    y = {f'target_{i}': [] for i in range(1, days_in_future + 1)}
-
-    for i in range(days_to_train, len(df)):
-        X.append(df[required_cols].iloc[i - days_to_train:i].values.flatten())
-        for j in range(1, days_in_future + 1):
-            y[f'target_{j}'].append(df[f'target_{j}'].iloc[i])
+    X_train, y_train, X_test, y_test, future_X = get_split_data(df_org, days_to_train, days_in_future)
     
-    X = np.array(X)
-    y = {k: np.array(v) for k, v in y.items()}
-
-    X_train, X_test = X[:-1], X[-1].reshape(1, -1)
-    y_train = {k: v[:-1] for k, v in y.items()}
-    y_test = {k: v[-1] for k, v in y.items()}
-    y_test = np.array([y_test[f'target_{i}'] for i in range(1, days_in_future + 1)])
-    
-    future_X = df_org[required_cols].values[-days_to_train:].flatten().reshape(1, -1)
 
     #train and predict 
     models = {}
@@ -57,8 +36,9 @@ def predict_stock_prices(
         models[f'model_day_{day}'] = model
 
         train_pred = model.predict(X_train[-1].reshape(1, -1))
-        loss = get_score(train_pred.reshape(1, -1), df_org['Close'].iloc[-days_in_future+day-2].reshape(1, -1), loss_function)
-
+        loss = get_score(train_pred.flatten(), df_org['Close'].iloc[-days_in_future+day-2].flatten(), loss_function)
+        print(train_pred.reshape(1, -1))
+        print(df_org['Close'].iloc[-days_in_future+day-2].reshape(1, -1))
         current_val_loss = get_score(pred.reshape(1, -1), y_test[[-days_in_future+day-1]].reshape(1, -1), loss_function)
     test_preds = np.array(test_preds)
     
@@ -73,4 +53,29 @@ def get_score(predicted, real, loss_function):
     elif loss_function == "mae":
         score = mean_absolute_error(predicted, real)
     return score
-        
+
+
+def get_split_data(df_org, days_to_train, days_in_future):
+    df = df_org.copy()
+    required_cols = df.columns
+    for i in range(1, days_in_future + 1):
+        df[f'target_{i}'] = df['Close'].shift(-i)
+    df.dropna(inplace=True)
+    
+    df_training = df.iloc[0:-days_to_train]
+    df_test =df.iloc[-days_to_train:]
+    X_train = []
+    y_train = {f'target_{i}': [] for i in range(1, days_in_future + 1)}
+
+    for i in range(days_to_train, len(df_training)):
+        X_train.append(df_training[required_cols].iloc[i - days_to_train:i].values.flatten())
+        for j in range(1, days_in_future + 1):
+            y_train[f'target_{j}'].append(df_training[f'target_{j}'].iloc[i])
+    
+    X_train = np.array(X_train)
+    y_train = {k: np.array(v) for k, v in y_train.items()}
+    X_test = np.array(df_test[required_cols]).flatten().reshape(1,-1)
+    y_test = np.array(df_org['Close'].iloc[-days_in_future:])
+    
+    future_X = df_org[required_cols].values[-days_to_train:].flatten().reshape(1, -1)
+    return X_train, y_train, X_test, y_test, future_X

visualization.py
@@ -15,7 +15,7 @@ def get_plot(
 ):
     if np.any(result_of_testing == None) :
         return None
-    df = df.tail(days_in_future_to_predict * 2)
+    df = df.tail(days_in_future_to_predict * 2+10)
 
     fig, ax = plt.subplots(figsize=(12, 6))
     ax.plot(df.index, df["Close"], label="Real price")